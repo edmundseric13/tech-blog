722259fef646040d1a70624b8a9c22df65525180
edmundseric13 <160555371+edmundseric13@users.noreply.github.com>
2024-07-29 18:53:09 -0600

    edited routes folders and added middleware

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { User, Post, Comment } = require('../../models');
 
 // Example route
 router.get('/', (req, res) => {
   res.send('Welcome to the Tech Blog API!');
 });
 
-// Add more routes here
-router.use('/api', require('./api'));
+// User routes
+router.use('/users', require('./userRoutes'));
+
+// Post routes
+router.use('/posts', require('./postRoutes'));
+
+// Comment routes
+router.use('/comments', require('./commentRoutes'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sequelize = require('./config/connection');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const routes = require('./controllers/api');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: process.env.SESSION_SECRET,
+  secret: 'your-secret-key',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -23,7 +22,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
